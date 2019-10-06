2e71f9193c76705151e2aad41ce13afcc2e54f4c
Gabriel Lisaca <gabriel.lisaca@gmail.com>
2019-10-06 15:24:46 +1300

    Add checks for required macro calls

--- atlint/main.py
+++ atlint/main.py
@@ -11,9 +11,9 @@ from .checks import CHECKERS
 
 PROGRAM_NAME = "atlint"
 
+
 def warn_general(message):
     print(f"{PROGRAM_NAME}: {message}")
-
 
 
 def main(argv=None):
@@ -28,7 +28,8 @@ def main(argv=None):
     elif os.path.exists("configure.in"):
         configure_file = "configure.in"
         warn_general(
-            "Files named 'configure.in' are deprecated. Consider renaming to 'configure.ac'")
+            "Files named 'configure.in' are deprecated. Consider renaming to 'configure.ac'"
+        )
     else:
         warn_general("Cannot find 'configure.ac' or 'configure.in'")
         # TODO: Use proper exit code
@@ -45,7 +46,16 @@ def main(argv=None):
         checker(matching_macros, context)
 
     # Checking existence of macros
-    pass
+    required_macro_calls = [
+        ("AC_CONFIG_AUX_DIR", ["[build-aux]"]),
+        ("AC_CONFIG_MACRO_DIR", ["[m4]"]),
+    ]
+    # Membership testing with sets is clearer (and usually faster) than nested for-loops.
+    present_calls = {macro.name for macro in macro_calls}
+    for exact_name, args in required_macro_calls:
+        if exact_name not in present_calls:
+            call_text = f"{exact_name}({', '.join(args)})"
+            warn_file(configure_file, f"Call to {call_text} should be present.")
 
     # Makefile.am checks
     # configure.ac + Makefile.am checks (cross-file knowledge)
